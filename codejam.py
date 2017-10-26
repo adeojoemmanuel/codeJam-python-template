27035c19e78a337a6d6562fbad191e635e79d8b7
adeojoemmanuel <emmanuel.adojo@yahoo.com>
2017-10-26 11:44:46 +0100

    updates

--- codejam.py
+++ codejam.py
@@ -1,14 +1,13 @@
 import sys
-count = 0
-file_object = open("input_file.txt", "r")
-thefile = open('test.txt', 'w')
-for line in file_object:
-	lines = int(line)
-	if (count == 0):
-		count += 1
-		continue
-	data = lines * 2
+OCC = 1
 
-	# sys.stdout=open("test.txt","w")
-	print("Case #{}: {}".format(count, lines))
-	# sys.stdout.close()
+fileContent = open("larg_input.txt", "r")
+writeFile = open("output.txt", "w")
+
+for line in fileContent:
+	currentLine = int(line)
+	newData = currentLine * 2
+	print("Case #{}: {}".format(OCC, newData))
+	writeFile.write("Case #{}: {}".format(OCC, newData) + '\n')
+
+	OCC += 1
